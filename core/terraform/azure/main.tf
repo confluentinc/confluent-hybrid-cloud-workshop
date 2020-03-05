70b6c70b9271cf3ff755720f515540eeec53eccb
Gianluca Natali <8244206+gianlucanatali@users.noreply.github.com>
2020-03-05 23:18:53 +0100

    adding Azure 2.0

--- core/terraform/azure/main.tf
+++ core/terraform/azure/main.tf
@@ -1,9 +1,10 @@
 provider "azurerm" {
-  version         = "~> 1.35"
+  version         = "=2.0.0"
   subscription_id = "${var.subscription_id}"
   client_id       = "${var.client_id}"
   client_secret   = "${var.client_secret}"
   tenant_id       = "${var.tenant_id}"
+  features {}
 }
 
 provider "random" {
