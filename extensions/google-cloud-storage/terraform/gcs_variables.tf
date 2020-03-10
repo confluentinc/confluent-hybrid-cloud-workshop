e97be6c387b176eb1623c3761c75351dfe7b1712
tjunderhill <tom.underhill7000@gmail.com>
2020-03-10 15:58:42 +0000

    removed unessesary comment blocks

--- extensions/google-cloud-storage/terraform/gcs_variables.tf
+++ extensions/google-cloud-storage/terraform/gcs_variables.tf
@@ -1,12 +1,3 @@
-# variable "name" {
-#   description = "The Prefix used for all resources in this example"
-# }
-
-# variable "participant_count" {
-#   description = "How many instances of this sample you want to create?"
-#   type        = number
-# }
-
 variable "gcs_project" {
   description = "GCP Project to use."
 }
@@ -14,7 +5,3 @@ variable "gcs_project" {
 variable "gcs_region" {
   description = "The region for the GCS bucket"
 }
-
-# variable "gcs_storage_class" {
-#   default = "REGIONAL"
-# }
