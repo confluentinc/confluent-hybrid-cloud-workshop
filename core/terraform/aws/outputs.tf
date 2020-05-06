668c30290edf4828b6760f69cbebbe77db76ef15
tjunderhill <tom.underhill7000@gmail.com>
2020-05-06 20:15:02 +0100

    output security_group_id

--- core/terraform/aws/outputs.tf
+++ core/terraform/aws/outputs.tf
@@ -1,3 +1,7 @@
 output "external_ip_addresses" {
   value = "${module.workshop-core.external_ip_addresses}"
 }
+
+output "security_group_id" {
+  value = "${module.workshop-core.security_group_id}"
+}
